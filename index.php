d1ffdb98f744cf1cd08ba9dd8c50d8a318a5297c
wch_mirek <mirek.g@webchefs.pl>
2020-11-27 10:29:36 +0100

    Server debug turned off

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-echo '<table>';
-foreach($_SERVER as $key => $value) {
-    echo '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
-}
-echo '</table>';
-exit;
+//echo '<table>';
+//foreach($_SERVER as $key => $value) {
+//    echo '<tr><td>' . $key . '</td><td>' . $value . '</td></tr>';
+//}
+//echo '</table>';
+//exit;
 
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
